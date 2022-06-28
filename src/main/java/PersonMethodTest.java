025d716e743ee926629e4068dbe2a36ed9589559
fengjian2705 <fengjian2705@sina.com>
2022-06-28 23:16:22 +0800

    feat: Method 类；

--- src/main/java/PersonMethodTest.java
+++ src/main/java/PersonMethodTest.java
@@ -10,6 +10,27 @@ public class PersonMethodTest {
         Object object = constructor.newInstance("jack", 18);
         Method m1 = c1.getMethod("getName");
         System.out.println(m1.invoke(object));
+
+        System.out.println("-----------");
+
+        Method[] methods = c1.getMethods();
+        for (Method method : methods) {
+            System.out.println("方法修饰符: "+ method.getModifiers());
+            System.out.println("方法返回值类型: "+ method.getReturnType());
+            System.out.println("方法名称是: "+ method.getName());
+            System.out.println("方法参数列表是: ");
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            for (Class<?> parameterType : parameterTypes) {
+                System.out.print(parameterType+" ");
+            }
+            System.out.println();
+            System.out.println("异常类型列表是: ");
+            Class<?>[] exceptionTypes = method.getExceptionTypes();
+            for (Class<?> exceptionType : exceptionTypes) {
+                System.out.print(exceptionType+" ");
+            }
+            System.out.println();
+        }
     }
 
 }
